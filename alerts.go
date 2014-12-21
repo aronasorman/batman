d8f4ef3656d61ecbf120cc2c50e6e7a82c9b6063
Aron Fyodor Asor <aronasorman@gmail.com>
2014-12-20 22:56:00 -0800

    make alert's exported json fields compliant with json standards

--- alerts.go
+++ alerts.go
@@ -5,9 +5,9 @@ import (
 )
 
 type Alert struct {
-	Id        int64
-	Location  string
-	Type      string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	Id        int64     `json:"id"`
+	Location  string    `json:"location"`
+	Type      string    `json:"type"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
