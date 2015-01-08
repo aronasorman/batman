40650034ec1464d7e805154d94756fe9c6f6466d
Aron Fyodor Asor <aronasorman@gmail.com>
2015-01-08 00:23:31 -0800

    add Alert.Source attribute

--- alerts.go
+++ alerts.go
@@ -5,9 +5,12 @@ import (
 )
 
 type Alert struct {
-	Id        int64     `json:"id"`
-	Location  string    `json:"location"`
-	Type      string    `json:"type"`
+	Id       int64  `json:"id"`
+	Location string `json:"location"`
+	Type     string `json:"type"`
+	// where the alert was retrieved; can be either rappler.com,
+	// PNP, or user entered.
+	Source    string    `json:"source"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
